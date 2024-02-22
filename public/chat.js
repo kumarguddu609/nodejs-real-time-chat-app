b7ec0396e719aa0166d1bd1ee1abbbe84a6ede6c
Guddu Kumar <kumarguddu609@gmail.com>
2024-02-22 14:29:19 +0530

    env

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://localhost:3700');
+    var socket = io.connect(process.env.SOCKET_URL);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
